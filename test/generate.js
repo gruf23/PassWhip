059a8c4b2748a1b99041b5c5a3ee7178ea44116c
gruf <gruf@codedrop.cc>
2021-08-11 13:26:04 +0600

    fix tests

--- test/generate.js
+++ test/generate.js
@@ -1,19 +1,19 @@
 const assert = require('chai').assert;
-const generatePassword = require('../index');
+const pwdGen = require('../lib/pwdgen');
 
 describe('Generate password', function () {
   it('Should generate password when no options provided', function () {
     assert.doesNotThrow(function () {
-      generatePassword();
+      pwdGen.generatePassword();
     });
   });
   it('Should generate password of provided length', function () {
     const length = 16;
-    const password = generatePassword({length: length});
+    const password = pwdGen.generatePassword({length: length});
     assert.equal(password.length, length);
   });
   it('Should generate password containing only letter', function () {
-    const password = generatePassword({
+    const password = pwdGen.generatePassword({
       numbers: false,
       symbols: false
     });
